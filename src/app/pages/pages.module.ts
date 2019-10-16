a55418f350a93d0f8216543d3f3851c8c3debe0a
Jaime Hernandez <hernandez.hs@gmail.com>
2019-10-16 15:37:17 -0300

    uso de atributos personalizados en angular

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { PAGES_ROUTES } from './pages.routes';
 
 import { SharedModule } from '../shared/shared.module';
 
+import {FormsModule} from '@angular/forms';
 
 import { PagesComponent } from './pages.component';
 
@@ -26,7 +28,8 @@ import { Graficas1Component } from './graficas1/graficas1.component';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 export class PagesModule { }
